5d4bd149938f8d4d8c486736c290998944635369
Rohyl <rohyljoshi@hotmail.com>
2019-06-15 01:44:05 +1000

    Express serves React app

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+export default () => {
+  return (
+    <>
+      <header>
+        <h1>Under Maintainence</h1>
+      </header>
+      <p>
+        Lookahead Semester Planner is{" "}
+        <a href="https://lookahead.rohyl.io/">here at lookahead.rohyl.io</a>
+      </p>
+    </>
+  );
+};
