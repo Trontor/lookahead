91528a766ec60cf5a19865e1da35b36c1f31c6c9
Rohyl <rohyljoshi@hotmail.com>
2019-06-29 12:30:50 +1000

    Exposed more Subject properties + simple Subject parse test written

--- test/SubjectClassScraper/SubjectClassScraper.ts
+++ test/SubjectClassScraper/SubjectClassScraper.ts
@@ -1,5 +1,4 @@
 import { expect } from "chai";
-import { readFile } from "fs";
 import "mocha";
 import { parseSubject } from "../../subject-utils/SubjectClassScraper";
 import { SubjectPeriod } from "../../subject-utils/SubjectPeriods";
@@ -14,10 +13,10 @@ describe("Subject Scraper", () => {
     );
     expect(subject).to.have.property("code", simpleSubjectJson.code);
     expect(subject).to.have.property("period", SubjectPeriod.Semester_1);
-    expect(subject)
-      .to.have.property("irregularClasses")
-      .that.deep.equals([]);
-    expect(subject.classes.length).to.equal(10);
+
+    expect(subject.regularClasses.length).to.equal(10);
     expect(subject.mandatoryClasses.length).to.equal(2);
+    expect(subject.streams.length).to.equal(0);
+    expect(subject.irregularClasses.length).to.equal(0);
   });
 });
