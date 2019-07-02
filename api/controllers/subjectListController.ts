b8cc65baf32ccea3c47d9ddf74ba8fe04e31fb0c
Rohyl <rohyljoshi@hotmail.com>
2019-07-02 18:07:11 +1000

    Added subjectList ENOENT info + fixed SubjectClassScraper test

--- api/controllers/subjectListController.ts
+++ api/controllers/subjectListController.ts
@@ -14,10 +14,11 @@ export const getSubjectList = (req: Request, res: Response) => {
     "../../subject-utils/subject-lists",
     filename
   );
+  console.log(subjectPath);
   // Check if the file exists, if so - send it back or return ENOENT
   fs.stat(subjectPath, err => {
     if (err) {
-      res.send("ENOENT");
+      res.send("ENOENT " + subjectPath);
     } else {
       res.header("Access-Control-Allow-Origin", "*");
       res.sendFile(subjectPath);
