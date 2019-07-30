cc57bb42020f0ea98e61998e2e792e4131cdafe3
Rohyl <rohyljoshi@hotmail.com>
2019-07-31 09:52:26 +1000

    [ADD] commenting to route controllers

--- api/controllers/clubListController.ts
+++ api/controllers/clubListController.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 import { getClubSheetData } from "../../google-sheets/sheets";
 
+/**
+ * Specifies how a sponsor object should look like, mimics the Google Sheets
+ * columns.
+ */
 interface ISponsor {
   name: string;
   logoURL: string;
@@ -11,9 +15,16 @@ interface ISponsor {
   facebook: string;
   include: string[];
 }
+
+/**
+ * Takes a raw Google Sheets response and parses rows into an array of ISponsor
+ */
 const processRawClubData = (data: string[][]): ISponsor[] => {
+  // will store output sponsors
   const allSponsors: ISponsor[] = [];
+  // loop through 2d data array
   for (const row of data) {
+    // destructure entries
     const [
       name,
       logoURL,
@@ -24,10 +35,13 @@ const processRawClubData = (data: string[][]): ISponsor[] => {
       tier,
       include
     ] = row;
+    // will store a list of subject code matching patterns
     let includeArray: string[] = [];
     if (include) {
+      // currently stored as in a CSV format in Google Sheets
       includeArray = include.split(",");
     }
+    // construct sponsor object and push to tracking array
     const sponsor: ISponsor = {
       name,
       logoURL,
@@ -42,6 +56,10 @@ const processRawClubData = (data: string[][]): ISponsor[] => {
   }
   return allSponsors;
 };
+
+/**
+ * Asyncronously retrieve and parse sponsorship data
+ */
 export const getClubs = async (req: Request, res: Response) => {
   const data = await getClubSheetData();
   res.send(processRawClubData(data));
--- api/controllers/subjectListController.ts
+++ api/controllers/subjectListController.ts
@@ -6,15 +6,15 @@ import path from "path";
  * Serves an appropriate subject list file given a year and study period.
  */
 export const getSubjectList = (req: Request, res: Response) => {
-  // The subject list file should be stored in such a format
+  // the subject list file should be stored in such a format
   const filename = `subjects_${req.query.year}_${req.query.period}.json`;
-  // The path to the subject list file (assuming it exists)
+  // the path to the subject list file (assuming it exists)
   const subjectPath = path.resolve(
     __dirname,
     "../../subject-utils/subject-lists",
     filename
   );
-  // Check if the file exists, if so - send it back or return ENOENT
+  // check if the file exists, if so - send it back or return ENOENT
   fs.stat(subjectPath, err => {
     if (err) {
       res.send("ENOENT " + subjectPath);
