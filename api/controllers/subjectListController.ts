c89203bd98a9039ba7bc363272aaf6a4b5b1bfae
Rohyl <rohyljoshi@hotmail.com>
2019-08-03 01:27:16 +1000

    [ADD] guards for path traversal for SubjectList

--- api/controllers/subjectListController.ts
+++ api/controllers/subjectListController.ts
@@ -1,13 +1,27 @@
 import { Request, Response } from "express";
 import * as fs from "fs";
 import path from "path";
+import { SubjectPeriod } from "../../subject-utils/SubjectPeriods";
 
 /**
  * Serves an appropriate subject list file given a year and study period.
  */
 export const getSubjectList = (req: Request, res: Response) => {
+  const { year, period } = req.query;
+  // Get valid semester periods from enum
+  const validPeriodValues = Object.keys(SubjectPeriod).map(
+    k => SubjectPeriod[k as any]
+  );
+  // Guards to prevent path traversal (thanks @josephsurin)
+  const validYear = !isNaN(year);
+  const validPeriod = validPeriodValues.some(val => val === period);
+  if (!validPeriod || !validYear) {
+    // in the case this was an attempt at a path traversal attack
+    res.status(404).send("Go Away");
+    return;
+  }
   // the subject list file should be stored in such a format
-  const filename = `subjects_${req.query.year}_${req.query.period}.json`;
+  const filename = `subjects_${year}_${period}.json`;
   // the path to the subject list file (assuming it exists)
   const subjectPath = path.resolve(
     __dirname,
