d10b827dcd82f85f3776628a8fe139b92181862c
Regnarox99 <mcbeanoo@gmail.com>
2021-01-20 16:06:36 +1100

    decreased size of time format button

--- client/src/components/Header/HeaderStyles.js
+++ client/src/components/Header/HeaderStyles.js
@@ -1,42 +1,42 @@
 import styled from "styled-components";
 
 export const HeaderWrapper = styled.div`
-    max-height: 60px;
-    min-height: 20px;
-    text-align: left;
-    background-color: ${(props) => props.theme.headerBg};
-    display: flex;
-    justify-content: center;
-    /* align-content: center; */
-    flex-direction: column;
-    color: white;
-    padding: 14px 12px;
-    margin: 0px;
-    /* Should always be on top7 */
-    z-index: 3;
-    position: relative;
-    box-shadow: 0 1px 2px 1px rgba(0, 0, 0, 0.12);
-    @media screen and (min-width: 1024px) {
-    }
+  max-height: 60px;
+  min-height: 20px;
+  text-align: left;
+  background-color: ${(props) => props.theme.headerBg};
+  display: flex;
+  justify-content: center;
+  /* align-content: center; */
+  flex-direction: column;
+  color: white;
+  padding: 14px 12px;
+  margin: 0px;
+  /* Should always be on top7 */
+  z-index: 3;
+  position: relative;
+  box-shadow: 0 1px 2px 1px rgba(0, 0, 0, 0.12);
+  @media screen and (min-width: 1024px) {
+  }
 `;
 
 export const HeaderText = styled.div`
-    letter-spacing: 0.02em;
-    text-transform: lowercase;
-    display: inline;
-    font-family: "Quicksand";
-    font-size: 24px;
-    font-weight: 800;
-    padding-bottom: 1px;
+  letter-spacing: 0.02em;
+  text-transform: lowercase;
+  display: inline;
+  font-family: "Quicksand";
+  font-size: 24px;
+  font-weight: 800;
+  padding-bottom: 1px;
 `;
 
 export const HeaderSubtitle = styled.span`
-    font-size: 11px;
-    display: inline;
-    text-transform: uppercase;
-    font-weight: bold;
-    letter-spacing: 0.08em;
-    opacity: 0.8;
+  font-size: 11px;
+  display: inline;
+  text-transform: uppercase;
+  font-weight: bold;
+  letter-spacing: 0.08em;
+  opacity: 0.8;
 `;
 
 export const TimeFormatButton = styled.span`
@@ -45,7 +45,7 @@ export const TimeFormatButton = styled.span`
     color: black;
     border: 2px solid white;
     border-radius: 5px;
-    padding: 4px 4px;
+    padding: 4px 2px;
     text-align: center;
     text-decoration: none;
     display: inline-block;
@@ -55,7 +55,7 @@ export const TimeFormatButton = styled.span`
     cursor: pointer;
 
     right: 70px;
-    font-size: 24px;
+    font-size: 18px;
     position: absolute;
     transform: translateY(2px);
     text-align: right;
@@ -70,14 +70,14 @@ export const TimeFormatButton = styled.span`
 `;
 
 export const Emoji = styled.span`
-    right: 12px;
-    font-size: 24px;
-    position: absolute;
-    transform: translateY(2px);
-    text-align: right;
-    cursor: pointer;
+  right: 12px;
+  font-size: 24px;
+  position: absolute;
+  transform: translateY(2px);
+  text-align: right;
+  cursor: pointer;
 
-    @media screen and (max-width: 359px) {
-        display: none;
-    }
+  @media screen and (max-width: 359px) {
+    display: none;
+  }
 `;
